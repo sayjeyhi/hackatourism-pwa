c839be7f8d2e9cbf40292be694d74e94a22534f5
jafarRezaei <bomber.man87@yahoo.com>
2020-01-31 19:29:27 +0330

    layout improve

--- app/components/Common/Layout/partials/Footer/styles.js
+++ app/components/Common/Layout/partials/Footer/styles.js
@@ -45,8 +45,24 @@ export const StyledFooterSocials = Styled.div`
   justify-content: space-around;
   width: 100%;
   margin-bottom: 10px;
-  a {
-    color: #ccc;
+  a * {
+    fill: #ccc;
+    transition: all ease-in-out 0.3s;
+  }
+  a.twitter:hover * {
+    fill: #00acee;
+  }
+  a.instagram:hover * {
+    fill: #7289f7;
+  }
+  a.medium:hover * {
+    fill: #00ab6c;
+  }
+  a.github:hover * {
+    fill: #333;
+  }
+  a.open-source:hover * {
+    fill: #6c93f0;
   }
   svg {
     width: 30px;
