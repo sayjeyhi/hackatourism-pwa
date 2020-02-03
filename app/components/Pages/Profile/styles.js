e7c3cc4e0023f7fcdd467fb216adf5ddd734db20
JafarRezaei <jafar.rezaei.ard@gmail.com>
2020-02-03 03:37:41 -0800

    -=fix home - add profiel

---
+++ app/components/Pages/Profile/styles.js
@@ -0,0 +1,16 @@
+import Styled from 'styled-components';
+import {Container} from "@snappmarket/ui";
+
+
+export const StyledProfileWrapper = Styled(Container)`
+  
+`;
+
+
+export const StyledProfileWrapper = Styled.aside`
+  flex: 0 0 25%;
+`;
+
+export const StyledProfileWrapper = Styled.div`
+  flex: 0 0 25%;
+`;
