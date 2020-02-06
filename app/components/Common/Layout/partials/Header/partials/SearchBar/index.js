da2d95fc8191fab98b09042dfe660bd7aa268839
jafarRezaei <bomber.man87@yahoo.com>
2020-02-06 12:25:06 +0330

    Header search add fonts

--- app/components/Common/Layout/partials/Header/partials/SearchBar/index.js
+++ app/components/Common/Layout/partials/Header/partials/SearchBar/index.js
@@ -4,15 +4,12 @@ import { NavLink, useHistory } from 'react-router-dom';
 
 import { useDebounce } from '@snappmarket/hooks';
 
+import Loading from 'components/Common/Loading';
 import routes from 'components/Common/Router/routes';
 import generalMessages from 'constants/Messages/general.messages';
 import SearchIcon from 'resources/svg/Icons/SearchIcon';
-// import PlusIcon from 'resources/svg/Icons/DeleteIcon';
-// import APP_INFO from 'constants/appInfo';
-import ThIcon from 'resources/svg/Icons/ThIcon';
+import APP_INFO from 'constants/appInfo';
 import { context as HeaderContext } from '../../context/headerContext';
-// import ResultNotFound from './ResultNotFound';
-import SearchingAnimation from './SearchingAnimation';
 import { StyledSearchBox, StyledSearchBoxHolder } from './styles';
 
 const SearchBar = () => {
@@ -63,47 +60,6 @@ const SearchBar = () => {
     }
   }, [debouncedSearchTerm]);
 
-  // const renderCategories = () =>
-  //   categories.length ? (
-  //     <>
-  //       <div className="part-title">
-  //         {generalMessages.header.search.categoriesPartTitle}
-  //       </div>
-  //       {categories.slice(0, 5).map(category => (
-  //         <NavLink
-  //           to={`${routes.search.path}/${searchQuery}/?categories=${category.id}`}
-  //           onClick={handleCloseSearchBoxPanel}
-  //           key={category.id}
-  //           title={category.title}
-  //         >
-  //           <ThIcon />
-  //           {category.title}
-  //         </NavLink>
-  //       ))}
-  //     </>
-  //   ) : null;
-  //
-  // const renderSuggestions = () =>
-  //   suggestions.length ? (
-  //     <>
-  //       <div className="part-title">
-  //         {generalMessages.header.search.suggestionPartTitle}
-  //       </div>
-  //       {suggestions.slice(0, 5).map(suggestion => (
-  //         <NavLink
-  //           to={`${routes.search.path}/${suggestion}`}
-  //           onClick={handleCloseSearchBoxPanel}
-  //           key={suggestion}
-  //           title={suggestion}
-  //         >
-  //           <ThIcon />
-  //           {suggestion}
-  //         </NavLink>
-  //       ))}
-  //     </>
-  //   ) : null;
-
-  // const nothingFound = !categories.length && !suggestions.length;
   return (
     <StyledSearchBox opened={false}>
       <label suppressHydrationWarning>
@@ -117,35 +73,23 @@ const SearchBar = () => {
           onKeyPress={handleEnterPress}
           autoComplete="off"
         />
-        {/*{showSearchResult && nothingFound ? (*/}
-        {/*  <PlusIcon*/}
-        {/*    onClick={handleCloseSearchBoxPanel}*/}
-        {/*    className="close-icon"*/}
-        {/*  />*/}
-        {/*) : (*/}
+
         <NavLink
           onClick={handleCloseSearchBoxPanel}
           to={`${routes.search.path}/${searchQuery}`}
         >
           <SearchIcon />
         </NavLink>
-        {/*)}*/}
       </label>
       <StyledSearchBoxHolder visible={false}>
-        {/*{getSearchResultStatus === APP_INFO.API_STATUSES.SUCCESS ? (*/}
-        {/*  <>*/}
-        {/*    {nothingFound ? (*/}
-        {/*      <ResultNotFound />*/}
-        {/*    ) : (*/}
-        {/*      <>*/}
-        {/*        {renderCategories()}*/}
-        {/*        {renderSuggestions()}*/}
-        {/*      </>*/}
-        {/*    )}*/}
-        {/*  </>*/}
-        {/*) : (*/}
-        <SearchingAnimation />
-        {/*)}*/}
+        {'SUCCESS' === APP_INFO.API_STATUSES.SUCCESS ? (
+          <>نتایج</>
+        ) : (
+          <>
+            <Loading />
+            <span>در حال جستجو</span>
+          </>
+        )}
       </StyledSearchBoxHolder>
     </StyledSearchBox>
   );
