8839d54e25f511219b53ef1071f19fc7f6357d49
jafarRezaei <bomber.man87@yahoo.com>
2020-02-06 14:33:10 +0330

    smart trip ui improvement

--- app/components/Pages/SmartTrip/index.js
+++ app/components/Pages/SmartTrip/index.js
@@ -2,25 +2,26 @@ import React from 'react';
 import SeoHead from 'components/Common/Seo/SeoHead';
 import { StyledSmartTripWrapper } from './styles';
 
-const AroundHere = () => {
-  return (
-    <>
-      <SeoHead title="سفر هوشمند" />
-      <StyledSmartTripWrapper>
-        <div className="from-city">
-          <select name="choose-from-city" id="choose-from-city">
-            <option value="1">
-              تهران
-            </option>
-            <option value="2">
-              کیش
-            </option>
-          </select>
-        </div>
-        <div className="to-city"></div>
-      </StyledSmartTripWrapper>
-    </>
-  );
-};
+const AroundHere = () => (
+  <>
+    <SeoHead title="سفر هوشمند" />
+    <StyledSmartTripWrapper className="flex-row justify-around">
+      <div className="from-city">
+        <label htmlFor="choose-from-city">از :</label>
+        <select name="choose-from-city" id="choose-from-city">
+          <option value="1">تهران</option>
+          <option value="2">کیش</option>
+        </select>
+      </div>
+      <div className="to-city">
+        <label htmlFor="choose-to-city">به :</label>
+        <select name="choose-to-city" id="choose-to-city">
+          <option value="1">تهران</option>
+          <option value="2">کیش</option>
+        </select>
+      </div>
+    </StyledSmartTripWrapper>
+  </>
+);
 
 export default AroundHere;
--- app/components/Pages/SmartTrip/styles.js
+++ app/components/Pages/SmartTrip/styles.js
@@ -5,9 +5,26 @@ export const StyledSmartTripWrapper = Styled.div`
   background: #ffffff94;
   width: 98%;
   border-radius: 20px;
-  height: 100%;
   position: absolute;
   top: 20px;
   z-index: -1;
   box-shadow: 0px 6px 34px -9px rgba(175,175,175,0.28);
+  > div {
+    padding-top: 50px;
+    flex: 0 0 calc(50% - 40px);
+    position: relative;
+    label {
+      font-size: 20px;
+      font-weight: 900;
+    }
+  }
+  
+  select {
+    background: #fff;
+    border-radius: 20px;
+    padding: 8px;
+    height: 42px;
+    border: 1px solid #d8d8d8 !important;
+    width: 100%;
+  }
 `;
